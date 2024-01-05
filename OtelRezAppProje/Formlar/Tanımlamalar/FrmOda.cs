6b7655212b578cdd8a67dd2d217f0eef1f08311d
gencyigitcan <yigitcangenc@gmail.com>
2024-01-05 23:57:15 +0300

    Oda Tanımları

---
+++ OtelRezAppProje/Formlar/Tanımlamalar/FrmOda.cs
@@ -0,0 +1,37 @@
+﻿using OtelRezAppProje.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtelRezAppProje.Formlar.Tanımlamalar
+{
+    public partial class FrmOda : Form
+    {
+        public FrmOda()
+        {
+            InitializeComponent();
+        }
+
+        Db_OtelRezAppEntities db = new Db_OtelRezAppEntities();
+
+        private void FrmOda_Load(object sender, EventArgs e)
+        {
+            db.TblOda.Load();
+            bindingSource1.DataSource = db.TblOda.Local;
+
+            repositoryItemLookUpEditDurum.DataSource = (from x in db.TblDurum
+                                                        select new
+                                                        {
+                                                            x.DurumId,
+                                                            x.DurumAd
+                                                        }).ToList();
+        }
+    }
+}
