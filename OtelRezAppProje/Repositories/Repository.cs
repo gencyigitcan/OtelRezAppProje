ebcf9433bcdab73eeb79093f1a388c9ad907b211
gencyigitcan <yigitcangenc@gmail.com>
2024-01-10 23:14:13 +0300

    Repository Design Pattern

---
+++ OtelRezAppProje/Repositories/Repository.cs
@@ -0,0 +1,37 @@
+﻿using OtelRezAppProje.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtelRezAppProje.Repositories
+{
+    public class Repository<T> where T : class, new()
+    {
+        Db_OtelRezAppEntities db = new Db_OtelRezAppEntities();
+
+        public List <T> GetAll()
+        {
+            return db.Set<T>().ToList();
+        }
+        public void TAdd(T p)
+        {
+            db.Set<T>().Add(p);
+            db.SaveChanges();
+        }
+        public void TDelete(T p)
+        {
+            db.Set<T>().Remove(p);
+            db.SaveChanges();
+        }
+        public T TGet(int id) 
+        {
+            return db.Set<T>().Find(id);
+        }
+        public void TUpdate()
+        {
+            db.SaveChanges();
+        }
+    }
+}
